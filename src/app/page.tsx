776343430c0d86fced5a7e2b7485b225e5bb0815
orangetechguy <orangetechguy@gmail.com>
2024-04-05 22:14:33 +0100

    homepage responsiveness first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,24 +16,24 @@ import { Badge } from "@/components/ui/badge";
 export default function Home() {
   return (
       <section>
-          <div className="homeHeroSection min-h-[70vh] ">
+          <div className="homeHeroSection min-h-[50vh] ">
 
           </div>
-          <div className="flex justify-center items-center flex-wrap gap-7 py-2 mt-[-70px] menuIconsContainer">
+          <div className="flex justify-center items-center flex-wrap gap-3 py-2 mt-[-60px] menuIconsContainer">
               <div className="flex flex-col justify-center items-center gap-2">
-                  <BsCashStack size={30} className="text-white" />
-                  <small className="text-white">Make Deposit</small>
+                  <BsCashStack size={20} className="text-white" />
+                  <small className="text-white text-sm">Make Deposit</small>
               </div>
               <div className="flex flex-col justify-center items-center gap-2">
-                  <BiMoneyWithdraw size={30} className="text-white"/>
+                  <BiMoneyWithdraw size={20} className="text-white"/>
                   <small className="text-white">Withdraw Money</small>
               </div>
               <div className="flex flex-col justify-center items-center gap-2">
-                  <RiContactsLine size={30} className="text-white" />
+                  <RiContactsLine size={20} className="text-white" />
                   <small className="text-white">Customer service</small>
               </div>
               <div className="flex flex-col justify-center items-center gap-2">
-                  <FaRegCalendarAlt size={30} className="text-white"/>
+                  <FaRegCalendarAlt size={20} className="text-white"/>
                   <small className="text-white">Daily Check-in</small>
               </div>
           </div>
@@ -42,17 +42,17 @@ export default function Home() {
           <div className=" my-10">
               <h1 className="text-xl text-center">My Account</h1>
               <div className="my-5 flex flex-wrap justify-center items-center gap-2">
-                  <Card className="homeAccountCard text-center w-[150px] h-[150px] rounded-md">
+                  <Card className="homeAccountCard text-center w-[120px] h-[120px] rounded-md">
                       <h1 className="text-xl font-bold text-white">&#8358;20,000</h1>
                       <small className="text-md text-white">Account Balance</small>
                       <div className="acOverlay"></div>
                   </Card>
-                  <Card className="homeAccountCard1 text-center w-[150px] h-[150px] rounded-md">
+                  <Card className="homeAccountCard1 text-center w-[120px] h-[120px] rounded-md">
                       <h1 className="text-xl font-bold text-white">&#8358;20,000</h1>
                       <small className="text-md text-white">Today&apos;s Earnings</small>
                       <div className="acOverlay"></div>
                   </Card>
-                  <Card className="homeAccountCard2 text-center w-[150px] h-[150px] rounded-md">
+                  <Card className="homeAccountCard2 text-center w-[120px] h-[120px] rounded-md">
                       <h1 className="text-xl font-bold text-white">&#8358;20,000</h1>
                       <small className="text-md text-white">Total Earnings</small>
                       <div className="acOverlay"></div>
